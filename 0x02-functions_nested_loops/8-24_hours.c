b24f65637e66f7c6de29b14a069d627e0e47adab
kelvinquarcoo <kelvinquarcoo2005@gmail.com>
2023-02-21 18:27:48 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			-putchar((hour / 10) + '0');
+			-putchar((hour % 10) + '0');
+			-putchar((':');
+			-putchar((minute / 10) + '0');
+			-putchar((minute % 10) + '0');
+			-putchar(('\n');
+		}
+	}
+}
