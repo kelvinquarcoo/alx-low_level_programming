fd4bb30747d49a709438bcb0d5641e00b832f9ef
kelvinquarcoo <kelvinquarcoo2005@gmail.com>
2023-02-23 10:25:53 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
