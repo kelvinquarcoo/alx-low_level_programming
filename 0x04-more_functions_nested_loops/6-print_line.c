0ea7e08d88d6f39e5ad9a86ec522c978ef778285
kelvinquarcoo <kelvinquarcoo2005@gmail.com>
2023-02-23 23:06:40 -0800

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,23 +3,21 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
- * Return: empty
  */
-
 void print_line(int n)
 {
-	int x;
-	
 	if (n <= 0)
 	{
-	_putchar('\n');
-	}
-	else
+		_putchar('\n');
+	} else
 	{
-	for (x = 0; x < n; x++)
-	{
-	_putchar(95);
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
+
 }
